faaf392ae21fcd8cf05390b389ad9244bc9e6495
SIMPLURIS\shassaninia <shassaninia@simpluris.com>
2017-03-15 11:07:46 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { QuestionService } from './question.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+    providers:  [QuestionService]
 })
 export class AppComponent {
-  title = 'app works!';
+ questions: any[];
+
+ constructor(service: QuestionService) {
+    this.questions = service.getQuestions();
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+    ReactiveFormsModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
